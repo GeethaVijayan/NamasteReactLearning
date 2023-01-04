60c00a95b517e96af8bdc8e28e0a44455c903d49
Geetha Vijayan <gitavijayan30@gmail.com>
2023-01-04 23:11:48 +0530

    firstcommit

---
+++ App.js
@@ -0,0 +1,13 @@
+ const heading = React.createElement("h1",{id:'title'},"heading1");                                
+const heading2 = React.createElement("h1",{id:'title'},"heading2");
+                                   
+                                            
+const parent = React.createElement("div",{id:"container"},[heading,heading2])                                
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+                                    
+                                
+                                
+                                
+                                
+                                
